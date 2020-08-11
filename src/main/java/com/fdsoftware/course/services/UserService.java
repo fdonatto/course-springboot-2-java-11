8a11b3cc6d46f1ed50956abbb9372020c1585036
fdonatto <fdonatto@gmail.com>
2020-08-11 20:11:12 -0300

    Exception handling - delete

--- src/main/java/com/fdsoftware/course/services/UserService.java
+++ src/main/java/com/fdsoftware/course/services/UserService.java
@@ -4,35 +4,44 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.fdsoftware.course.entities.User;
 import com.fdsoftware.course.repositories.UserRepository;
+import com.fdsoftware.course.services.exceptions.DatabaseException;
 import com.fdsoftware.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
-		return userRepository.findAll();		
+
+	public List<User> findAll() {
+		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User user) {
 		return userRepository.save(user);
 	}
-	
+
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User user) {
 		User entity = userRepository.getOne(id);
 		updateData(entity, user);
@@ -42,6 +51,6 @@ public class UserService {
 	private void updateData(User entity, User user) {
 		entity.setName(user.getName());
 		entity.setEmail(user.getEmail());
-		entity.setPhone(user.getPhone());		
+		entity.setPhone(user.getPhone());
 	}
 }
